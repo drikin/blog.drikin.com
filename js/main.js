4b7cab52f915651c24d3f1e050d6e742ffe67037
drikin <drikin@gmail.com>
2014-01-22 22:19:09 -0800

    the latest design

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var tw_username_regexp = /([^a-zA-Z0-9_]|^|RT:?)([@＠]+)([a-zA-Z0-9_]{1,20})/g;
+var tw_hashtag_regexp = /#([a-zA-Z0-9]+)/g;
+
+
+function linkUsernames(text) {
+    return text.replace(
+    tw_username_regexp,
+        '@<a class="username" href="http://twitter.com/$3">$3</a>');
+}
+
+function linkHashtags(text) {
+    return text.replace(
+        tw_hashtag_regexp,
+        '<a class="hashtag" href="http://twitter.com/search?q=$1">#$1</a>'
+    );
+}
+
+$(document).ready(function () {
+    $('p').each(function () {
+        $(this).html(linkUsernames($(this).html()));
+        //$(this).html(linkHashtags($(this).html()));
+    });
+});
 
