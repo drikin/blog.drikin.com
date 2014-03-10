67f9be8893ccd316e8a149b0ccf2ca5d0d85b6c3
drikin <drikin@gmail.com>
2014-03-09 20:23:16 -0700

    improve twitter name link script

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+var html_tag_regexp = /(<\/?[^>]+>)|([\s!-;=-~]+)/g;
 var tw_username_regexp = /([^a-zA-Z0-9_]|^|RT:?)([@＠]+)([a-zA-Z0-9_]{1,20})/g;
 var tw_hashtag_regexp = /#([a-zA-Z0-9]+)/g;
 
 
 function linkUsernames(text) {
-    return text.replace(
-    tw_username_regexp,
-        '@<a class="username" href="http://twitter.com/$3">$3</a>');
+    var replaced = text.replace(html_tag_regexp, function(){
+        // hit for html tags
+        if(arguments[1]) {
+            return arguments[1];
+        }
+        if(arguments[2]) {
+            var t = arguments[2];
+            return t.replace(tw_username_regexp,
+                '@<a class="username" href="http://twitter.com/$3">$3</a>');
+        }
+    });
+    return replaced;
 }
 
 function linkHashtags(text) {
